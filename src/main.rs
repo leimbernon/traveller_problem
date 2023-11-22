92f58546fc673b9e93f2ba80dfd0b1558d663b5f
Luis Eduardo Imbernón <imbernon@gmail.com>
2023-11-22 16:36:09 +0000

    Added the adaptive genetic algorithms

--- src/main.rs
+++ src/main.rs
@@ -77,13 +77,12 @@ fn main() {
         process::exit(1);
     }else{
 
-        //We initialize the population and the configuration
+        //We get the alleles
         let alleles = csv_read.unwrap();
-        let genes_per_individual = alleles.len();
 
-        //We run genetic algorithms
+        //We run genetic algorithms without adaptive genetic algorithms
         let start = Instant::now();
-        let best_population: Population<Genotype> = Ga::new()
+        let best_population_without_aga: Population<Genotype> = Ga::new()
             .with_threads(8)
             .with_logs(LogLevel::Info)
             .with_max_generations(1000)
@@ -94,17 +93,45 @@ fn main() {
             .with_crossover_method(Crossover::Cycle)
             .with_mutation_method(Mutation::Swap)
             .with_survivor_method(Survivor::Fitness)
-            .with_alleles(alleles)
-            .with_genes_per_individual(genes_per_individual as i32)
+            .with_alleles(alleles.clone())
+            .with_genes_per_individual(alleles.len() as i32)
             .with_alleles_can_be_repeated(false)
             .with_population_size(100)
             .run();          
         let duration = start.elapsed();
 
-        println!("Best fitness: {}", best_population.individuals[0].fitness);
+        //We run genetic algorithms with adaptive genetic algorithms
+        let start = Instant::now();
+        let best_population_with_aga: Population<Genotype> = Ga::new()
+            .with_threads(8)
+            .with_logs(LogLevel::Info)
+            .with_max_generations(1000)
+            .with_problem_solving(ProblemSolving::Minimization)
+            .with_best_individual_by_generation(true)
+            .with_number_of_couples(10)
+            .with_selection_method(Selection::Tournament)
+            .with_crossover_method(Crossover::Cycle)
+            .with_mutation_method(Mutation::Swap)
+            .with_survivor_method(Survivor::Fitness)
+            .with_alleles(alleles.clone())
+            .with_genes_per_individual(alleles.len() as i32)
+            .with_alleles_can_be_repeated(false)
+            .with_population_size(100)
+            .with_adaptive_ga(true)
+            .with_crossover_probability_min(0.1)
+            .with_crossover_probability_max(0.9)
+            .run();          
+        let aga_duration = start.elapsed();
+
+
+        //Write the results
+        println!("Best fitness: {}", best_population_without_aga.individuals[0].fitness);
         println!("Time elapsed in genetic algorithms() is: {:?}", duration);
 
+        println!("Best aga fitness: {}", best_population_with_aga.individuals[0].fitness);
+        println!("Time elapsed in adaptive genetic algorithms() is: {:?}", aga_duration);
+
         //We write the plot with the results
-        write_plot(best_population);
+        write_plot(best_population_without_aga);
     }
 }
